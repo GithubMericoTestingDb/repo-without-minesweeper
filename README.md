dff9d1190807a342636a1b3fb41c6e042a2e65a0
Hezheng Yin <hezheng@meri.co>
2020-12-13 23:31:41 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+## Criteria for minesweeper
+
+When a function's cyclomatic complexity goes from above or equals to 10 to below 10.
+
+## Test cases
+
+1. test1 has an intial cc of 12, and goes down to 11 in the second commit
+2. test2 has an intial cc of 9, and goes down to 8 in the second commit
+
---
+++ main.py
@@ -0,0 +1,43 @@
+
+
+ def test1():
+    if a:
+        return a
+    elif b:
+        return b
+    elif c:
+        return c
+    elif d:
+        return d
+    elif e:
+        return e
+    elif f:
+        return f
+    elif g:
+        return g
+    elif h:
+        return h
+    elif i:
+        return i
+    elif j:
+        return j
+    elif k:
+        return k
+
+def test2():
+    if a:
+        return a
+    elif b:
+        return b
+    elif c:
+        return c
+    elif d:
+        return d
+    elif e:
+        return e
+    elif f:
+        return f
+    elif g:
+        return g
+    elif h:
+        return h
